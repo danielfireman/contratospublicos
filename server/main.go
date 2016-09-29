0fe36ec2569a7c5dd596792e3179e70b82c70b83
Daniel Fireman <danielfireman@gmail.com>
2016-09-29 15:52:01 -0300

    Colocar o servidor para ouvir a porta

--- server/main.go
+++ server/main.go
@@ -13,10 +13,11 @@ func main() {
 	if port == "" {
 		log.Fatal("Variável de ambiente $PORT não encontrada.")
 	}
-	log.Println("Porta utilizada: ", port)
-
+	
 	router := httprouter.New()
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		fmt.Fprintf(w, "Hellow World.")
 	})
+	log.Println("Serviço inicializado na porta ", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
